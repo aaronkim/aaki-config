29f5b5a7abb5bc23ff24d266d7169b6a26ddad88
aaronkim <builingul@gmail.com>
2014-11-06 22:08:31 +0900

    oneshot build

--- cmake/aaki-root.cmake
+++ cmake/aaki-root.cmake
@@ -2,7 +2,6 @@ message("Welcome Aaki's root.")
 
 message("Root : " ${search_dir})
 set(ENV{AAKI_CFG.TEMP} "temp" PARENT_SCOPE)
-set(ENV{AAKI_CFG.FILE} $ENV{AAKI_CFG.ROOT}/aaki-config/cmake/aaki-config.cmake)
-include($ENV{AAKI_CFG.FILE} OPTIONAL)
+set(ENV{AAKI_CFG.CFG_DIR} $ENV{AAKI_CFG.ROOT}/aaki-config/cmake)
+include($ENV{AAKI_CFG.CFG_DIR}/aaki-config.cmake OPTIONAL)
 
-set(ENV{AAKI_CFG} "fixed" PARENT_SCOPE)
